606ec2af4c7092ed54a5d00b827bd963884a64e4
niravshah <nirav.shah83@gmail.com>
2015-11-28 00:50:59 +0000

    Service Selector 2

--- app/src/main/java/com/demandnow/ServiceSelectorActivity.java
+++ app/src/main/java/com/demandnow/ServiceSelectorActivity.java
@@ -15,6 +15,7 @@ import java.util.List;
  */
 public class ServiceSelectorActivity extends GDNBaseActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

faa9ce43ca610713d437379c5277a2cfbbb11cf0
niravshah <nirav.shah83@gmail.com>
2015-11-28 19:29:42 +0000

    Service Selector 3

--- app/src/main/java/com/demandnow/adapters/ServiceSelectorAdapter.java
+++ app/src/main/java/com/demandnow/adapters/ServiceSelectorAdapter.java
@@ -1,12 +1,15 @@
 package com.demandnow.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.demandnow.MainActivity;
 import com.demandnow.R;
+import com.demandnow.SharedPrefrences;
 import com.demandnow.model.ContactInfo;
 
 import java.util.List;
@@ -31,9 +34,7 @@ public class ServiceSelectorAdapter extends RecyclerView.Adapter<ServiceSelector
     public void onBindViewHolder(ContactViewHolder contactViewHolder, int i) {
         ContactInfo ci = contactList.get(i);
         contactViewHolder.vName.setText(ci.name);
-        contactViewHolder.vSurname.setText(ci.surname);
-        contactViewHolder.vEmail.setText(ci.email);
-        contactViewHolder.vTitle.setText(ci.name + " " + ci.surname);
+        contactViewHolder.info = ci;
     }
 
     @Override
@@ -47,16 +48,19 @@ public class ServiceSelectorAdapter extends RecyclerView.Adapter<ServiceSelector
 
     public static class ContactViewHolder extends RecyclerView.ViewHolder {
         protected TextView vName;
-        protected TextView vSurname;
-        protected TextView vEmail;
-        protected TextView vTitle;
+        protected ContactInfo info;
 
         public ContactViewHolder(View v) {
             super(v);
             vName = (TextView) v.findViewById(R.id.txtName);
-            vSurname = (TextView) v.findViewById(R.id.txtSurname);
-            vEmail = (TextView) v.findViewById(R.id.txtEmail);
-            vTitle = (TextView) v.findViewById(R.id.title);
+
+            v.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    SharedPrefrences.setCurrentService(info.getName());
+                    v.getContext().startActivity(new Intent(v.getContext(), MainActivity.class));
+                }
+            });
         }
     }
 }
